31257668033558368432f4a8782fd61562e4ef97
ShedrackGodson <hedrickgodson23@gmail.com>
2020-08-20 23:54:36 +0300

    configuring static files

--- core/views.py
+++ core/views.py
@@ -5,4 +5,4 @@ from .models import Order, OrderItem, Item
 def item_list(request):
     context = dict()
     context["items"] = Item.objects.all()
-    return render(request, "item_list.html", context)
+    return render(request, "home-page.html", context)
