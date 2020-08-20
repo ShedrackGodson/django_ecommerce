a6879326b76900d597b939471093078217a4f0cb
ShedrackGodson <hedrickgodson23@gmail.com>
2020-08-21 01:16:22 +0300

    base template customized done

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
-from .views import item_list
+from .views import item_list, products, checkout
 from django.urls import path
 
 app_name = 'core'
 
 urlpatterns = [
     path('', item_list, name="item_list"),
+    path('checkout/', checkout, name="checkout"),
+    path('product/', products, name="product"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from .models import Order, OrderItem, Item
+# from django.core.urlresolvers import resolve
 
 
 def item_list(request):
     context = dict()
     context["items"] = Item.objects.all()
     return render(request, "home-page.html", context)
+
+
+def products(request):
+
+    return render(request, "product-page.html", {
+
+    })
+
+
+def checkout(request):
+    
+    return render(request, "checkout-page.html", {
+        
+    })
