7eb147cdd094fe4cb733cfd525a25c06db61b3a1
ShedrackGodson <hedrickgodson23@gmail.com>
2020-08-22 00:40:44 +0300

    increment the quantity of single item in the cart

--- core/views.py
+++ core/views.py
@@ -60,54 +60,23 @@ def add_to_cart(request, slug):
         if order.item.filter(item__slug=item.slug).exists():
             order_item.quantity += 1
             order_item.save()
-            messages.success(
-                request, "This Item quantity is updated successfully.", fail_silently=False
-            )
+            # messages.success(
+            #     request, "This Item quantity is updated successfully.", fail_silently=False
+            # )
+            return redirect("core:order-summary")
         
         else:
             order.item.add((order_item))
             messages.success(request,
             "You have successfully added an item to the cart.", fail_silently=False
             )
+            return redirect("core:order-summary")
 
     else:
         order = Order.objects.create(user=request.user, ordered_date=timezone.now())
         order.item.add(order_item)
     
-    return redirect("core:product", slug=slug)
-
-
-@login_required
-def add_single_item_to_cart(request, slug):
-    item = get_object_or_404(Item, slug=slug)
-    order_item, created = OrderItem.objects.get_or_create(
-        item=item,
-        user=request.user,
-        ordered=False
-    )
-    order_qs = Order.objects.filter(user=request.user, ordered=False)
-
-    if order_qs.exists():
-        order = order_qs[0]
-        # check if the order item is in the order
-        if order.item.filter(item__slug=item.slug).exists():
-            order_item.quantity += 1
-            order_item.save()
-            messages.success(
-                request, "This Item quantity is updated successfully.", fail_silently=False
-            )
-        
-        else:
-            order.item.add((order_item))
-            messages.success(request,
-            "You have successfully added an item to the cart.", fail_silently=False
-            )
-
-    else:
-        order = Order.objects.create(user=request.user, ordered_date=timezone.now())
-        order.item.add(order_item)
-    
-    return redirect("core:product", slug=slug)
+    return redirect("core:order-summary")
 
 
 @login_required
@@ -140,7 +109,7 @@ def remove_from_cart(request, slug):
         else:
             # Adding a message saying that the order doesn't contain this OrderItem
             messages.info(request, "Your Order doesn't contains an Item.", fail_silently=False)
-            return redirect("core:product", slug=slug)
+            return redirect("core:product")
 
     else:
         # Adding a message saying a user doesnt have an order
@@ -174,10 +143,11 @@ def remove_single_item_from_cart(request, slug):
 
             order_item.quantity -= 1
             order_item.save()
-            messages.success(
-                request, "This item quantity was updated.", fail_silently=False
-                )
-        
+            # messages.success(
+            #     request, "This item quantity was updated.", fail_silently=False
+            #     )
+            return redirect("core:order-summary")
+
         else:
             # Adding a message saying that the order doesn't contain this OrderItem
             messages.info(request, "Your Order doesn't contains an Item.", fail_silently=False)
