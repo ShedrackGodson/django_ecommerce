a6de8f3130ffe3e9ff8350ac77f89770886cfb33
ShedrackGodson <hedrickgodson23@gmail.com>
2020-08-22 00:28:26 +0300

    prremove single item from the cart

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from .views import checkout, HomeView, ItemDetailView, add_to_cart,remove_from_cart, \
-    OrderSummaryView
+    OrderSummaryView,remove_single_item_from_cart
 from django.urls import path
 
 app_name = 'core'
@@ -11,4 +11,5 @@ urlpatterns = [
     path('product/<slug>/detail/', ItemDetailView.as_view(), name="product"),
     path('add-to-cart/<slug>/', add_to_cart, name="add-to-cart"),
     path('remove-from-cart/<slug>/', remove_from_cart, name="remove-from-cart"),
+    path('remove-item-from-cart/<slug>/', remove_single_item_from_cart, name="remove-single-item-from-cart"),
 ]
--- core/views.py
+++ core/views.py
@@ -78,6 +78,39 @@ def add_to_cart(request, slug):
 
 
 @login_required
+def add_single_item_to_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_item, created = OrderItem.objects.get_or_create(
+        item=item,
+        user=request.user,
+        ordered=False
+    )
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.item.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+            messages.success(
+                request, "This Item quantity is updated successfully.", fail_silently=False
+            )
+        
+        else:
+            order.item.add((order_item))
+            messages.success(request,
+            "You have successfully added an item to the cart.", fail_silently=False
+            )
+
+    else:
+        order = Order.objects.create(user=request.user, ordered_date=timezone.now())
+        order.item.add(order_item)
+    
+    return redirect("core:product", slug=slug)
+
+
+@login_required
 def remove_from_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
     # print(item.slug)
@@ -108,6 +141,47 @@ def remove_from_cart(request, slug):
             # Adding a message saying that the order doesn't contain this OrderItem
             messages.info(request, "Your Order doesn't contains an Item.", fail_silently=False)
             return redirect("core:product", slug=slug)
+
+    else:
+        # Adding a message saying a user doesnt have an order
+        messages.info(request, "You don't seems to have an Order yet!.", fail_silently=False)
+        return redirect("core:product", slug=slug)
+
+    return redirect("core:product", slug=slug)
+
+
+@login_required
+def remove_single_item_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    # print(item.slug)
+
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False
+    )
+
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        # print(order.item.filter(item__slug=item.slug))
+        if order.item.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            # print("Exists.")
+
+            order_item.quantity -= 1
+            order_item.save()
+            messages.success(
+                request, "This item quantity was updated.", fail_silently=False
+                )
+        
+        else:
+            # Adding a message saying that the order doesn't contain this OrderItem
+            messages.info(request, "Your Order doesn't contains an Item.", fail_silently=False)
+            return redirect("core:order-summary", slug=slug)
 
     else:
         # Adding a message saying a user doesnt have an order
