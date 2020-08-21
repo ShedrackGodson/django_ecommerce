97157bbf2758997446d875b3b6626fdc05e194cd
ShedrackGodson <hedrickgodson23@gmail.com>
2020-08-21 19:20:47 +0300

    cart_count template done

--- core/templatetags/cart_template_tags.py
+++ core/templatetags/cart_template_tags.py
@@ -5,8 +5,8 @@ register = template.Library()
 
 
 @register.filter
-def cart_item_count(user):
-    if user.is_authenticated():
+def cart_item_count(user)->int:
+    if user.is_authenticated:
         qs = Order.objects.filter(user=user, ordered=False)
         if qs.exists():
             return qs[0].item.count()
