ae07eb74bd783cd278b0c9e587fc32f26097d398
ShedrackGodson <hedrickgodson23@gmail.com>
2020-08-22 03:07:20 +0300

    checkout form customization

--- core/forms.py
+++ core/forms.py
@@ -2,11 +2,23 @@ from django import forms
 from django_countries.fields import CountryField
 
 
+
+PAYMENT_OPTION = (
+    ('S', 'Stripe'),
+    ('P', 'PayPal')
+)
+
 class CheckoutForm(forms.Form):
-    street_address = forms.CharField()
-    appartment_address = forms.CharField(required=False)
-    country = CountryField(blank_label='(select country)')
-    zip_code = forms.CharField()
-    save_info = forms.BooleanField(widget=forms.CheckboxInput())
-    same_billing_address = forms.BooleanField(widget=forms.CheckboxInput())
-    payment_option = forms.BooleanField(widget=forms.RadioSelect())
+    street_address = forms.CharField(widget=forms.TextInput(attrs={
+        "placeholder": "123 Mount St"
+    }))
+    appartment_address = forms.CharField(required=False, widget=forms.TextInput(attrs={
+        "placeholder": "Appartment or suite"
+    }))
+    country = CountryField(blank_label='(select country)').formfield()
+    zip_code = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "form-control"
+    }))
+    save_info = forms.BooleanField(widget=forms.CheckboxInput(), required=False)
+    same_billing_address = forms.BooleanField(widget=forms.CheckboxInput(), required=False)
+    payment_option = forms.ChoiceField(widget=forms.RadioSelect(), choices=PAYMENT_OPTION)
